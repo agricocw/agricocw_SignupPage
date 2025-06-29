a6f0a69978f3514799b1482f98b6a11e93bc20c8
cwagr <agricocw@mail.uc.edu>
2025-06-28 21:21:35 -0400

    Made edits to the ProfilePage.xaml and cs files

--- agricocw_SignupPage/ProfilePage.xaml.cs
+++ agricocw_SignupPage/ProfilePage.xaml.cs
@@ -1,9 +1,38 @@
 namespace agricocw_SignupPage;
 
+[QueryProperty(nameof(Username), "username")]
+[QueryProperty(nameof(Email), "email")]
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
-	{
-		InitializeComponent();
-	}
+    public ProfilePage()
+    {
+        InitializeComponent();
+    }
+
+    private string username;
+    public string Username
+    {
+        get => username;
+        set
+        {
+            username = Uri.UnescapeDataString(value);
+            UsernameLabel.Text = username;
+        }
+    }
+
+    private string email;
+    public string Email
+    {
+        get => email;
+        set
+        {
+            email = Uri.UnescapeDataString(value);
+            EmailLabel.Text = email;
+        }
+    }
+
+    private async void OnSignOutClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//signup");
+    }
 }
